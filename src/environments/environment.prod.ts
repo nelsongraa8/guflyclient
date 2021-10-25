fc324558a39367720c7d3bcb7dd11a9c8e64c21a
nelsongraa8 <nelsongraa8@gmail.com>
2021-10-25 04:56:02 -0400

    cambiando de repositorio del backend, planeando como hacer un cambio automatico en el repositorio segun la fecha del mes y documentarlo bien

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  url: 'https://app-gufly.herokuapp.com',
+  //url: 'https://app-gufly.herokuapp.com',
+  url: 'https://tranquil-waters-60366.herokuapp.com',
 };
