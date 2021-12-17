e0ce66ca67d0feecc913eca9ad22638786c74f7a
Nelson R. Graña <87838742+nelsongraa8@users.noreply.github.com>
2021-12-16 23:18:36 -0500

    Cambios en el servidor de peliculas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //url: 'https://app-gufly.herokuapp.com',
-  url: 'https://tranquil-waters-60366.herokuapp.com',
+  url: 'https://app-gufly.herokuapp.com',
+  //url: 'https://tranquil-waters-60366.herokuapp.com',
 };
